cc8f393cf7cc802f9752a1b7a1e0a67705eeec4f
Kamori <kamori.goat@gmail.com>
2016-05-15 22:32:50 -0500

    few more links need fixed

--- include/process_reset.php
+++ include/process_reset.php
@@ -54,13 +54,13 @@ else {
 }
 
 ?>
-<link href="css/bootstrap.css" rel="stylesheet">
-<link href="style.css" rel="stylesheet">
+<link href="../css/bootstrap.css" rel="stylesheet">
+<link href="../style.css" rel="stylesheet">
 
 <!-- jQuery (necessary for Bootstraps JavaScript plugins) -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 <!-- Include all compiled plugins (below), or include individual files as needed -->
-<script src="js/bootstrap.min.js"></script>
+<script src="../js/bootstrap.min.js"></script>
 
 <script type="text/JavaScript" src="../js/sha512.js"></script>
 <script type="text/JavaScript" src="../js/forms.js"></script>
