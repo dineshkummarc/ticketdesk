7c2d8d4693bc62dcdf7b0586c8876e8358aa2c73
Justin Hanley <joevegan@gmail.com>
2016-09-16 21:20:16 -0600

    sql test

--- include/class.report.php
+++ include/class.report.php
@@ -2,7 +2,7 @@
 include_once('connect.php');
 
 class report {
-	
+
 	private $mysqli;
 	private $startDate;
 	private $endDate;
@@ -12,11 +12,11 @@ class report {
 	private $subCategory;
 	private $status;
 	private $name;
-	
+
 	function __construct() {
 		$this->mysqli = dbConnect();
 	}
-	
+
 	// gets
 	public function getMysqli() {return $this->mysqli;}
 	public function getStartDate() {return $this->startDate;}
@@ -28,8 +28,6 @@ class report {
 	public function getSubCategory() {return $this->subCategory;}
 	public function getStatus() {return $this->status;}
 
-	
-	
 	// sets
 	public function setMysqli($mysqli) { $this->mysqli = $mysqli;}
 	public function setStartDate($startDate) {$this->startDate = $startDate;}
@@ -40,51 +38,41 @@ class report {
 	public function setCategory($category) { $this->category = $category;}
 	public function setSubCategory($subCategory) {$this->subCategory = $subCategory;}
 	public function setStatus($status) { $this->status = $status;}
-	
-	
+
+
 	public function createReport() {
-	 	  
+
 	    $output = fopen('php://output', 'w');
 
 	    // output the column headings
-	    fputcsv($output, array('TicketId','clientId','opendate', 'opened_by','assigned_to','details','Status','Category','SubCategory','Notes'));
+	    fputcsv($output, array('TicketId','clientId','open_date', 'opened_by','assigned_to','details','Status','Category','SubCategory','Notes'));
 
-	      	$sql = "select 
+	    $sql = "select
 			t.id,
-			t.clientnumber, 
+			t.clientnumber,
 			t.opendate,
-			t.user, 
-			t.assigneduser, 
-			t.comments, 
+			t.user,
+			t.assigneduser,
+			t.comments,
 			ts.status,
 			ts.statusdate,
-			tn.note, 
+			tn.note,
 			tn.notedate
 				from tickets t, ticketnotes tn, categories c, subcategories sc,ticketstatus ts
 					where t.id = tn.ticketid
 					and ts.ticketid = t.id
 					and t.categoryid = c.id
 					and t.subcategoryid = sc.id
-						order by t.opendate desc,tn.notedate";
-				
-		$result = $this->mysqli->query($sql);
-		
+						order by t.opendate desc, tn.notedate";
+        $sql = "select * from tickets";
+        $result = $this->mysqli->query($sql);
+
 		if ($result->num_rows > 0) {
-			// output data of each row
-			while($row = $result->fetch_assoc()) {
-		       		fputcsv($output, $row);
-			}
+			while($row = $result->fetch_assoc()) { fputcsv($output, $row);	}
 		}
-		
-	   
-	    mysqli_close($this->mysqli);  
-	
-	
-	
+	  mysqli_close($this->mysqli);
+
 	}
-	
-
-
 }
 
 
