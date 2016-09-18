d24247a51e49f6c82673eef63e860044f1b10643
Justin Hanley <joevegan@gmail.com>
2016-09-18 10:58:46 -0600

    sys brand settings fix

--- include/navbar.php
+++ include/navbar.php
@@ -34,7 +34,7 @@ error_reporting(E_ALL);
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="main.php"><?php echo ''. $systemName->getValue(); ?></a>
+            <a class="navbar-brand" href="main.php"><?php echo ''. $systemBrand->getValue(); ?></a>
         </div>
 
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
